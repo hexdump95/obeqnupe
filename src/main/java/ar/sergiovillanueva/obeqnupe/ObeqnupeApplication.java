db91998a511d1bef02df082a88440dd723a0357e
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-07-28 13:12:14 -0300

    first commit

---
+++ src/main/java/ar/sergiovillanueva/obeqnupe/ObeqnupeApplication.java
@@ -0,0 +1,13 @@
+package ar.sergiovillanueva.obeqnupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObeqnupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObeqnupeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/sergiovillanueva/obeqnupe/ObeqnupeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.sergiovillanueva.obeqnupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObeqnupeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
